92e57ea2872c4eda856b00b7290c89c0a6f20f1f
zhongziqi233 <2673122425@qq.com>
2022-12-06 03:21:57 +0800

    title exchange

--- etw/src/App.vue
+++ etw/src/App.vue
@@ -2,24 +2,24 @@
   <!-- eslint-disable vue/no-deprecated-slot-attribute -->
   <div id="main">
     <div id="left" class="block">
-      <Panel :title="'Rank'" :panelHeight="'50%'" :panelWidth="'100%'" style="float:left" :titleStyle="{'height':titleHeight+'px','line-height':titleHeight+'px'}">
+      <Panel :title="'全国热门景点销量排行'" :panelHeight="'50%'" :panelWidth="'100%'" style="float:left" :titleStyle="{'height':titleHeight+'px','line-height':titleHeight+'px'}">
         <template v-slot:panelBody>
           <RankView></RankView>
         </template>
       </Panel> 
-      <Panel :title="'Bar'" :panelHeight="'50%'" :panelWidth="'100%'" style="float:left" :titleStyle="{'height':titleHeight+'px','line-height':titleHeight+'px'}">
+      <Panel :title="'全国各省市4A-5A景区数量'" :panelHeight="'50%'" :panelWidth="'100%'" style="float:left" :titleStyle="{'height':titleHeight+'px','line-height':titleHeight+'px'}">
         <template v-slot:panelBody>
           <BarView></BarView>
         </template>
       </Panel>
     </div>
     <div id="right" class="block">
-      <Panel :title="'Map'" :panelHeight="'50%'" :panelWidth="'100%'" style="float:left" :titleStyle="{'height':titleHeight+'px','line-height':titleHeight+'px'}">
+      <Panel :title="'全国各省市假期出行数据地图'" :panelHeight="'50%'" :panelWidth="'100%'" style="float:left" :titleStyle="{'height':titleHeight+'px','line-height':titleHeight+'px'}">
         <template v-slot:panelBody>
           <MapView></MapView>
         </template>
       </Panel>
-      <Panel :title="'Cloud'" :panelHeight="'50%'" :panelWidth="'100%'" style="float:left" :titleStyle="{'height':titleHeight+'px','line-height':titleHeight+'px'}">
+      <Panel :title="'全国各景点简介文本统计词云图'" :panelHeight="'50%'" :panelWidth="'100%'" style="float:left" :titleStyle="{'height':titleHeight+'px','line-height':titleHeight+'px'}">
         <template v-slot:panelBody>
           <CloudView></CloudView>
         </template>
